4e4518691f51bf059b27758d4452b98f0ff170fd
NINJA-J <1605563934@qq.com>
2017-09-21 19:25:11 +0800

    some changes

---
+++ ejbModule/org/bjut/j2ee/P1T7/DataResourceApplication/DBManager.java
@@ -0,0 +1,14 @@
+package org.bjut.j2ee.P1T7.DataResourceApplication;
+
+public class DBManager {
+
+	public DBManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
